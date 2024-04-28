e420a4572025bdce382592c4d47171c3fe6c3177
Techterrasentinel <89346054+Techterrasentinel@users.noreply.github.com>
2024-04-28 23:49:07 +0100

    public without history v2

---
+++ README.md
@@ -0,0 +1,5 @@
+This is an instant runoff vote counter designed to work with a string of values including an --END BALLOT-- marker and a ";" to seperate names of people.
+
+This is Ideal for dealing with the outputs of a microsoft form outputted to excel spreadsheet.
+
+Coded by Tom for the TTG AGM elections on 28th may 2024
---
+++ main.py
@@ -0,0 +1,102 @@
+
+# Toms vote counter
+
+class Voter:  # The class for each instance of a voter
+    def __init__(self, input_line: str):
+        self.ballot: list[str] = []
+        self.ballot = input_line.split(";")  # values from Excel are separated by ";", split them into list
+        self.ballot = self. ballot[:(self. ballot.index("--END BALLOT--"))]  # removes all candidates beyond then end
+
+    def remove_candidate(self, candidate) -> int:
+        """Remove the stated candidate from the voters ballot, if they exist."""
+        if candidate in self.ballot:  # only remove candidate if they exist
+            self. ballot.remove(candidate)
+            return 1
+        else:
+            return 0
+
+    def return_ballot(self) -> list[str]:
+        """Return the whole ballot, this is a debug function."""
+        return self.ballot
+
+    def return_first(self) -> str | None:
+        """Return the current first choice of the voter."""
+        if len(self. ballot) > 0:
+            return self. ballot[0]
+        else:
+            return None
+
+
+class Election:
+    def __init__(self, input_line: str, _voters: list[Voter]):
+        self.candidates: list[str] = input_line.split(";")  # split down to get candidates
+        self.candidates.pop(len(self.candidates)-1)  # remove double quote at the end, that weirdly appears
+        self.candidates.remove("--END BALLOT--")  # makes the list proper
+        self.round = 0  # counts the rounds of voting
+        self.active_candidates: dict[str, int] = {}
+        self.voters: list[Voter] = _voters
+
+        for entry in self.candidates:  # Create the instant of the candidates and their vote counts
+            self.active_candidates[entry] = 0
+
+    def tally_votes(self) -> str:
+        """Tally up the votes for all candidates, print the outputs and remove the lowest candidate."""
+        for candidate in self.active_candidates:  # clear all vote tallies
+            self.active_candidates[candidate] = 0
+
+        lost_votes: int = 0
+        self.round += 1
+        for current_voter in self. voters:
+            entry = current_voter.return_first()
+            if entry is None or not self.active_candidates.__contains__(entry):
+                lost_votes += 1
+            else:
+                self.active_candidates[entry] += 1
+
+        print("\n")
+        print(f"During round {self. round} of voting the tallies are {self.active_candidates}.")
+        print(f"Currently {lost_votes} votes have been lost.")
+
+        lowest_candidate = ""
+        lowest_votes: int = 1000  # set arbitrarily large value so it will be replaced
+        for candidate in self.active_candidates:
+            count = self.active_candidates[candidate]
+            if count < lowest_votes:
+                lowest_votes = count
+                lowest_candidate = candidate
+
+        self.active_candidates.pop(lowest_candidate)
+        for voter in self.voters:
+            voter.remove_candidate(lowest_candidate)
+
+        print(f"Lowest candidate {lowest_candidate} with {lowest_votes} has been removed.")
+
+        if len(self.active_candidates) == 1:
+            print(f"{self.active_candidates} is the winner!")
+            return "Finish"
+        else:
+            print("Proceeding to next round of voting.")
+            return "Continue"
+
+
+# Main code Stuff
+
+
+file = open("votes.txt", "r")  # open the file storing the list of votes
+
+all_candidates = file.readline().rstrip("\n")  # get all possible candidates as each voter has entered them all
+file.seek(0)  # reset index of pointer in file
+
+all_voters: list[Voter] = []
+for line in file:  # instantiate all voters
+    all_voters.append(Voter(line.rstrip("\n")))
+
+file.close()  # close file as good practice
+
+this_election = Election(all_candidates, all_voters)  # create the whole election
+
+flag_over = True
+while flag_over:
+    if this_election.tally_votes() != "Continue":
+        flag_over = False
+
